8ee017f68069f8f67fe98dc3ccef1f11f11ba61e
DanielleCarmi09 <daniellecarmi@gmail.com>
2025-08-03 17:53:27 +0300

    Ex6

--- src/src/RecordEx.java
+++ src/src/RecordEx.java
@@ -1,10 +1,11 @@
 public class RecordEx {
+
     public record Student(String name, int grade, int classNum) {
         public Student(String name, int classNum){
             this(name, 9, classNum);
         }
         public String Information(){
-            String info = "student " + this.name + " is in " + this.grade + "grade in class number " + this.classNum;
+            String info = "student " + this.name + " is in " + this.grade + "th grade in class number " + this.classNum;
             return info;
         }
         public boolean isInHighschool(){
@@ -12,5 +13,15 @@ public class RecordEx {
             else return false;
         }
 
+
+
+    }
+    public static void ThisStudent(String name, int grade, int classNum){
+        Student s = new Student(name, grade, classNum);
+        System.out.println(s.Information());
+        System.out.println(s.isInHighschool());
+    }
+    public static void main(String[] args){
+        ThisStudent("Danielle", 11, 11);
     }
 }
