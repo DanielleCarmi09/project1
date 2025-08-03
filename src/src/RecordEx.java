6f8c4715e7b3e6424b41548e2ff9d870ec026126
DanielleCarmi09 <daniellecarmi@gmail.com>
2025-08-03 17:45:42 +0300

    Ex5

--- src/src/RecordEx.java
+++ src/src/RecordEx.java
@@ -1,5 +1,8 @@
 public class RecordEx {
     public record Student(String name, int grade, int classNum) {
+        public Student(String name, int classNum){
+            this(name, 9, classNum);
+        }
         public String Information(){
             String info = "student " + this.name + " is in " + this.grade + "grade in class number " + this.classNum;
             return info;
