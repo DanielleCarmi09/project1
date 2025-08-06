f190751d726311a26ea48eaefda90fd190ac2dca
DanielleCarmi09 <daniellecarmi@gmail.com>
2025-08-06 17:04:11 +0300

    classes

--- src/src/DanielleConstants.java
+++ src/src/DanielleConstants.java
@@ -1,16 +1,8 @@
 public class DanielleConstants{
-    private static final int SIZE = 39;
-    private static final String FAVORITE_COLOR = "pink";
-    private static final double HIEGHEST_PRICE = 300;
-    private static final double TOLERANCE = 50;
+    protected static final int SIZE = 39;
+    protected static final String FAVORITE_COLOR = "pink";
+    protected static final double HEIGHEST_PRICE = 300;
+    protected static final double TOLERANCE = 50;
 
-    public static void main(String[]args){
-        Bed bed = new Bed(1.2,2, "white", 200);
-        Shirt[]shirts ={new Shirt(39, "red"), new Shirt(2, "blue")};
-        Room room = new Room(shirts, bed);
-        room.getBed().setSheetColor(FAVORITE_COLOR);
-        Shirt[] shirtsBySize = room.getCloset().getShirtsBySize(SIZE);
-        for (int i = 0; i<shirtsBySize.length;i++) System.out.println(shirtsBySize[i].getColor());
 
-    }
 }
