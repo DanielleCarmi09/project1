4d7df8699c30c6d1b01ca8e526c1801b03b1043f
DanielleCarmi09 <daniellecarmi@gmail.com>
2025-08-10 03:00:09 +0300

    enum

--- src/src/Product.java
+++ src/src/Product.java
@@ -5,7 +5,7 @@ public enum Product {
     WATER_BOTTLE("waterBottle",3),
     OIL_BOTTLE("oilBottle",10),
     SUGAR("sugar",7),
-    Apples("apples",0);
+    APPLES("apples",0);
 
     private String name;
     private int price;
@@ -13,13 +13,13 @@ public enum Product {
         this.name = name;
         this.price = price;
     }
-    public void setApplePrice(Scanner reader){
+    public static void setApplePrice(Scanner reader){
         System.out.println("What price are the apples by weight?");
         int price = reader.nextInt();
-        Apples.price = price;
+        APPLES.price = price;
     }
 
-    public int fullPrice(Product[]products, int[]amounts){
+    public static int fullPrice(Product[]products, int[]amounts){
         int price=0;
         for(int i=0; i<products.length;i++){
             price+=products[i].price*amounts[i];
