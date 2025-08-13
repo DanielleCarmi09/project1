306c31c262017ecae6396c083ed118abb0d8b8cf
DanielleCarmi09 <daniellecarmi@gmail.com>
2025-08-13 15:57:46 +0300

    genericsEx1

---
+++ src/src/ArrayList.java
@@ -0,0 +1,37 @@
+public class ArrayList<T>{
+    private Object[] List;
+    public ArrayList(Object[]List){
+        this.List = List;
+    }
+
+    public void add(T item){
+        int index = 0;
+        while(this.List[index]!=null || index <this.List.length){
+            index++;
+        }
+        if(index == this.List.length-1){
+            this.List = new Object[this.List.length*2];
+            this.List[index] = item;
+        }else{
+            this.List[index] = item;
+        }
+    }
+
+    public Object get(int index){
+        return this.List[index];
+    }
+
+    public void set(T item, int index){
+        this.List[index] = item;
+    }
+
+    public Object[] getList(){ return this.List;}
+
+    public static void printArrayList(ArrayList list){
+        for(int i = 0; i<list.getList().length;i++){
+            if(list.getList()[i]!=null){
+                System.out.println(list.getList()[i]);
+            }
+        }
+    }
+}
