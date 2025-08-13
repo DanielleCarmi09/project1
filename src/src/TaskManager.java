e286eed737925dbb5c7e2a900ff5e89bba205f80
DanielleCarmi09 <daniellecarmi@gmail.com>
2025-08-13 21:46:58 +0300

    lambdaEx5

--- src/src/TaskManager.java
+++ src/src/TaskManager.java
@@ -29,6 +29,32 @@ public class TaskManager{
     }
 
     public void deleteFilter(TaskFilter taskFilter){
-
+        int count =0;
+        for(int i=0; i<index+1;i++){
+            if(!taskFilter.isFiltered(this.tasks[i])) count++;
+        }
+        Task[] newTasks = new Task[count+1];
+        int index = 0;
+        for(int i=0; i<tasks.length;i++){
+            if(!taskFilter.isFiltered(this.tasks[i])){
+                newTasks[index] =this.tasks[i];
+                index++;
+            }
+        }
+        this.tasks = newTasks;
+        this.index = index;
     }
+
+    public void setForFiltered(int newImportance, TaskFilter taskFilter){
+        for(int i=0;i<index+1;i++){
+            if(taskFilter.isFiltered(this.tasks[i])) this.tasks[i].setImportance(newImportance);
+        }
+    }
+    public void setForNotFiltered(int newImportance, TaskFilter taskFilter){
+        for(int i=0;i<index+1;i++){
+            if(!taskFilter.isFiltered(this.tasks[i])) this.tasks[i].setImportance(newImportance);
+        }
+    }
+
+
 }
