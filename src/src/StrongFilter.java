6b1068484ec03c5ddedd0b3f003e681d47a6191e
DanielleCarmi09 <daniellecarmi@gmail.com>
2025-08-17 19:36:41 +0300

    advancedExercises

--- src/src/StrongFilter.java
+++ src/src/StrongFilter.java
@@ -1,4 +1,20 @@
-public class StrongFilter<T> {
-    boolean getFilterResult(T value);
+public interface StrongFilter<T>{
 
+    boolean getFilterResult(T value);
+    default <E extends T> StrongFilter<E> and(StrongFilter<E> filter, E value){
+        return new StrongFilter<E>() {
+            @Override
+            public boolean getFilterResult(E value) {
+                return this.getFilterResult(value) && filter.getFilterResult(value);
+            }
+        };
+    }
+    default <E extends T> StrongFilter<E> or(StrongFilter<E> filter, E value){
+        return new StrongFilter<E>() {
+            @Override
+            public boolean getFilterResult(E value) {
+                return this.getFilterResult(value) || filter.getFilterResult(value);
+            }
+        };
+    }
 }
