2e9850883fb56cbe5b8d2b9d3fb917a2621c89bf
DanielleCarmi09 <daniellecarmi@gmail.com>
2025-08-20 21:07:07 +0300

    advancedExercisesEx2

--- src/src/CharComparables.java
+++ src/src/CharComparables.java
@@ -1,9 +1,18 @@
-public class CharComparables implements Comparable<Character>{
+public record CharComparables(char a, char b) implements Comparable<CharComparables>{
 
     @Override
-    public int compareTo(){
-        if(this.A>b) return 1;
-        else if (a == b) return 0;
+    public int compareTo(CharComparables comparable){
+        if(this.a>comparable.a) return 1;
+        else if (this.a == comparable.a){
+            if (this.b>comparable.b) return 1;
+            else if (this.b==comparable.b) return 0;
+            else return -1;
+        }
         else return -1;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + a + ", " + b + ")";
     }
 }
--- src/src/SortByComparables.java
+++ src/src/SortByComparables.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class SortByComparables{
 
     public static <T extends Comparable<T>> void sort(T []arr){
@@ -13,5 +15,22 @@ public class SortByComparables{
             arr[i] = arr[minIndex];
             arr[minIndex] = temp;
         }
+    }
+
+    public static void main(String[]args){
+        CharComparables a = new CharComparables('a', 'b');
+        CharComparables b = new CharComparables('e', 'f');
+        CharComparables c = new CharComparables('a', 'd');
+        CharComparables d = new CharComparables('c', 'c');
+        CharComparables e = new CharComparables('d', 'e');
+        CharComparables f = new CharComparables('b', 'h');
+        CharComparables g = new CharComparables('c', 'c');
+        CharComparables h = new CharComparables('c', 'd');
+        CharComparables i = new CharComparables('b', 'b');
+        CharComparables j = new CharComparables('d', 'e');
+        CharComparables[] CharComparablesArr = {a, b, c, d, h, g, h, i, j};
+        System.out.println(Arrays.toString(CharComparablesArr));
+        SortByComparables.sort(CharComparablesArr);
+        System.out.println(Arrays.toString(CharComparablesArr));
     }
 }
